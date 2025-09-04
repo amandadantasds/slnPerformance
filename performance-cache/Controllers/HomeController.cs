ed3ca37d634bc3d0cc3e0d633b63fef024b634c1
Amanda Dantas <78170677+amandadantasds@users.noreply.github.com>
2025-09-03 22:26:00 -0300

    Update HomeController.cs

--- performance-cache/Controllers/HomeController.cs
+++ performance-cache/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
-﻿using Dapper;
+using Dapper;
 using Microsoft.AspNetCore.Mvc;
 using MySqlConnector;
+using Newtonsoft.Json;
+using StackExchange.Redis;
 using System.Threading.Tasks;
 
 namespace performance_cache.Controllers
@@ -12,11 +14,27 @@ namespace performance_cache.Controllers
         [HttpGet]
         public async Task<IActionResult> Get()
         {
+            string key = "get-users";
+            var redis = ConnectionMultiplexer.Connect("localhost:6379");
+            IDatabase db = redis.GetDatabase();
+
+            await db.KeyExpireAsync(key, TimeSpan.FromSeconds(20));
+
+            String userValue = await db.StringGetAsync(key);
+
+            if (!string.IsNullOrEmpty(userValue))
+            {
+                return Ok(userValue);
+            }
             using var connection = new MySqlConnection("Server=localhost;database=fiap;User=root;Password=123");
             await connection.OpenAsync();
 
             string sql = "SELECT id, name, email FROM users; ";
             var users = await connection.QueryAsync<Model.Users>(sql);
+            var userJson = JsonConvert.SerializeObject(users);
+            await db.StringSetAsync(key, userJson);
+
+            Thread.Sleep(3000);
 
             return Ok(users);
         }
